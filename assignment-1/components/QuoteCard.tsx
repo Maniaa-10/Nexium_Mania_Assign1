0a52e65ca0752ab5a393b0f58ea28c47e53f23da
Mania Shakeel <maniashakeel10@gmail.com>
2025-07-06 15:43:46 +0500

    Improved QuoteCard styling with hover effects

--- assignment-1/components/QuoteCard.tsx
+++ assignment-1/components/QuoteCard.tsx
@@ -8,7 +8,8 @@ export default function QuoteCard({ text }: QuoteCardText) {
       initial={{ opacity: 0, scale: 0.9, y: 40 }}
       animate={{ opacity: 1, scale: 1, y: 0 }}
       transition={{ duration: 0.75, ease: "easeOut" }}
-      className="bg-white/10 p-4 rounded-lg text-white italic backdrop-blur-sm shadow-md max-w-xl"
+      className= "bg-white/10 hover:bg-white/30 p-4 rounded-lg text-white hover:text-black italic backdrop-blur-sm shadow-md max-w-xl transition-all hover:scale-110"
+
     >
       “{text}”
     </motion.div>
